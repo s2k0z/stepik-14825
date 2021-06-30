4904b5d50090b08a5fe354303f8980543b29e155
Sergei K <s2k0z@outlook.com>
2021-06-30 10:06:40 +0500

    Имя симлинка конфига Nginx

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo rm -f /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/boxweb.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/boxweb
 sudo /etc/init.d/nginx restart

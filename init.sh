92a8a6c1abbf1bd6cd519b4a68897362f0dba07f
Sergei K <s2k0z@outlook.com>
2021-07-01 13:03:27 +0500

    Инициализация gunicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/boxweb
 sudo /etc/init.d/nginx restart
